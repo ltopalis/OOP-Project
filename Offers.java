fa4cdd4523e8502818e00dcb0b7f29b4cf63f62a
ltopalis <83336644+ltopalis@users.noreply.github.com>
2021-05-24 18:41:20 +0300

    Update Offers.java

--- Offers.java
+++ Offers.java
@@ -1,3 +1,14 @@
-public class Offers {
-    
+public class Offers extends RequestDonationList {
+    public boolean commit(Organization organization){
+        try{
+            for(int i = 0; i<super.getArrayList().size(); i++){
+                organization.addDonation(getArrayList().get(i));
+                if(!(super.remove(i))) return false;
+            }
+            return true;
+        }catch(Exception e){
+            System.err.println(e);
+            return false;
+        }
+    }
 }
