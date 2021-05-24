da44b4de02111434c73718180eb64e0b4708eedc
ltopalis <83336644+ltopalis@users.noreply.github.com>
2021-05-24 18:40:36 +0300

    Update and rename EntityAlreadyExists.java to EntityAlreadyExistsException.java

--- EntityAlreadyExists.java
+++
@@ -1,5 +0,0 @@
-public class EntityAlreadyExists extends OurExceptions{
-    public EntityAlreadyExists(){
-        super("This entity already exists in that list");
-    }
-}
---
+++ EntityAlreadyExistsException.java
@@ -0,0 +1,5 @@
+public class EntityAlreadyExistsException extends OurExceptions{
+    public EntityAlreadyExistsException(){
+        super("This entity already exists in that list");
+    }
+}
