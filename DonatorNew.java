69e4c9f23991c7dceeb95d0874115c5b92db08c9
iamnasos <84036457+iamnasos@users.noreply.github.com>
2021-05-27 18:37:00 +0300

    Create DonatorNew.java

---
+++ DonatorNew.java
@@ -0,0 +1,22 @@
+public class Donator extends User{ 
+    // LISTS:
+    private Offers offersList = new Offers();
+    
+    // CONSTRUCTORS:
+    public Donator(String name, String phone){
+    super(name, phone);     
+    }
+    
+    // WRAPPER METHODS FOR LIST MANAGEMENT:
+    
+    // #1 offersList (from class Offers)
+    public void addOffer(RequestDonation rd, Organization org){ 
+        this.offersList.add(rd, org, null); // adds a RequestDonation in list
+    }
+    public void removeOffer(RequestDonation rd){
+        this.offersList.remove(rd.getEntity()); // removes RequestDonation from list
+    }
+    public Offers getOffersList(){
+        return offersList;
+    }
+}
