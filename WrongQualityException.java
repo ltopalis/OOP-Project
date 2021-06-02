e2dc08aabb3969ecc6d07095644196fd075d23a7
ltopalis <83336644+ltopalis@users.noreply.github.com>
2021-06-02 14:46:27 +0300

    Delete WrongQualityException.java

--- WrongQualityException.java
+++
@@ -1,9 +0,0 @@
-public class WrongQuantityException extends OurExceptions {
-    public WrongQuantityException(){
-        super("The quantity you desire is invalid");
-    }
-
-    public WrongQuantityException(String msg){
-        super(msg);
-    }    
-}
