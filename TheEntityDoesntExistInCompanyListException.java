a57c1fe7f0090c463428ca16928d273359dddc0d
ltopalis <83336644+ltopalis@users.noreply.github.com>
2021-06-02 14:47:56 +0300

    Delete TheEntityDoesntExistInCompanyListException.java

--- TheEntityDoesntExistInCompanyListException.java
+++
@@ -1,5 +0,0 @@
-public class TheEntityDoesntExistInCompanyListException extends OurExceptions{
-    public TheEntityDoesntExistInCompanyListException(){
-        super("The Entity you try to add is not supported by the company!");
-    }
-}
