940cd9dd633f29e81810516607a3d4f056e11291
iamnasos <84036457+iamnasos@users.noreply.github.com>
2021-06-02 14:08:33 +0300

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -6,7 +6,7 @@ public class Menu{
  //============================ ΔΕΝ ΤΟ ΕΧΩ ΤΡΕΞΕΙ ΑΚΟΜΗ ΚΑΙ ΜΠΟΡΕΙ ΝΑ ΑΛΛΑΞΕΙ =====================
  public void AdminMenu(Scanner sc, Admin admin, Organization org){
 	//Εμφανίζει χαιρετισμό, τα στοιχεία του χρήστη και ένδειξη ότι είναι Admin.
-	System.out.println("----- Καλώς Ορίσατε " + admin.getName() + "! -----")
+	System.out.println("----- Καλώς Ορίσατε " + admin.getName() + "! -----");
 	System.out.println("Τηλ: " + admin.getPhone());
 	System.out.println("----- Έχετε συνδεθεί ως διαχειριστής. -----");
 	System.out.println("Οργανισμός: " + org.getName());
@@ -42,7 +42,7 @@ public class Menu{
         		System.out.println("4. Exit");
         		// =================================
         		System.out.print("Επιλογή? ");
-        		System("=======================================")
+        		System("=======================================");
 
             }
 
@@ -78,7 +78,7 @@ private int AdminChoice(Scanner sc) throws InputMismatchException{
 // #1 VIEW:
 private void view(Scanner sc, Admin admin, Organization org){
 	// show the categories first:
-	System.out.println("Παρακαλώ επιλέξτε είδος:")
+	System.out.println("Παρακαλώ επιλέξτε είδος:");
 	System.out.println("1. MATERIALS");
 	System.out.println("2. SERVICES");
 	System.out.println("3. ΑΚΥΡΩΣΗ");
@@ -91,7 +91,7 @@ private void view(Scanner sc, Admin admin, Organization org){
 			for(int i = 0; i < org.getCurrentDonations().listSize(); i++){
 				if(org.getCurrentDonations().getArrayList().get(i) instanceof Material)
 					System.out.println(org.getCurrentDonations().getArrayList().get(i),getEntity().getName() + " (" 
-						org.getCurrentDonations().getArrayList().get(i).getQuantity() + ")");
+						+ org.getCurrentDonations().getArrayList().get(i).getQuantity() + ")");
 						/* ^ για όλα τα στοιχεία τύπου Material εμφάνισε <ονομα του Entity> (<ποσότητα>)*/
 			}// end for loop #1
 			System.out.println("Εισάγετε το όνομα του <MATERIAL> όπως αυτό εμφανίζεται στην οθόνη για να εμφανίσετε περισσότερες πληροφορίες...");
@@ -112,7 +112,7 @@ private void view(Scanner sc, Admin admin, Organization org){
 			for(int i = 0; i < org.getCurrentDonations().listSize(); i++){
 				if(org.getCurrentDonations().getArrayList().get(i) instanceof Service)
 					System.out.println(org.getCurrentDonations().getArrayList().get(i),getEntity().getName() + " (" 
-						org.getCurrentDonations().getArrayList().get(i).getQuantity() + ")");
+						+ org.getCurrentDonations().getArrayList().get(i).getQuantity() + ")");
 						/* ^ για όλα τα στοιχεία τύπου Service εμφάνισε <ονομα του Entity> (<ποσότητα>)*/
 			}// end for loop #1
 			System.out.println("Εισάγετε το όνομα του <SERVICE> όπως αυτό εμφανίζεται στην οθόνη για να εμφανίσετε περισσότερες πληροφορίες...");
@@ -133,7 +133,7 @@ private void view(Scanner sc, Admin admin, Organization org){
 		default:
 			System.err.println("Λάθος επιλογή. Ξαναπροσπαθήστε!");
 			System.out.println("----------------------------------");
-			System.out.println("Παρακαλώ επιλέξτε είδος:")
+			System.out.println("Παρακαλώ επιλέξτε είδος:");
 			System.out.println("1. MATERIALS");
 			System.out.println("2. SERVICES");
 			System.out.println("3. ΑΚΥΡΩΣΗ");
@@ -144,16 +144,17 @@ private void view(Scanner sc, Admin admin, Organization org){
 
 // #2 MONITOR ORGANIZATION:
 private void monitorOrganization(Scanner sc, Admin admin, Organization org){
-	System.out.println("Παρακαλώ επιλέξτε την ενέργεια που επιθυμείτε να εκτελέσετε:")
+	System.out.println("Παρακαλώ επιλέξτε την ενέργεια που επιθυμείτε να εκτελέσετε:");
 	System.out.println("1. LIST BENEFICIARIES");
 	System.out.println("2. LIST DONATORS");
 	System.out.println("3. RESET BENEFICIARIES' LISTS");
 	System.out.println("4. ΑΚΥΡΩΣΗ");
 	// get input:
 	int choice;
+	int indexBen; // shows the index (the place) of the beneficiary inside organizations beneficiaryList (ex. 3. Josh, indexBen = 3)
 	switch(choice){
 		// LIST BENEFICIARIES:
-		int indexBen; // shows the index (the place) of the beneficiary inside organizations beneficiaryList (ex. 3. Josh, indexBen = 3)
+		
 	case 1:
 		org.listBeneficaries();
 		System.out.println("Επιλογή <BENEFICIARY>. Εισάγετε την ΑΡΙΘΜΗΣΗ του εποφελούμενου που θέλετε να επιλέξετε...");
@@ -166,7 +167,7 @@ private void monitorOrganization(Scanner sc, Admin admin, Organization org){
 		}
 			if ((indexBen < org.getBeneficiaryList().size()) && (indexBen >= 0)) {
 				Beneficiary selectedBen = org.getBeneficiaryList().get(i); // temporary reference to the selected beneficiary 
-				System.out.println("Συνολικές Παροχές:")
+				System.out.println("Συνολικές Παροχές:");
  				for(int j = 0; j < org.getBeneficiaryList().get(i).getReceivedList().listSize(); j++){
 					System.out.println(selectedBen.getReceivedList().getArrayList().get(j).getEntity().getEntityInfo() + " (" +selectedBen.getReceivedList().getArrayList().get(j).getQuantity() + ")"); // prints out each received list (entity quantity)
 				}// end receivedList loop	
@@ -174,7 +175,7 @@ private void monitorOrganization(Scanner sc, Admin admin, Organization org){
 			else 
 				System.out.println("Δεν βρέθηκε...");
 
-		System.out.println("Παρακαλώ επιλέξτε την ενέργεια που επιθυμείτε να εκτελέσετε:")
+		System.out.println("Παρακαλώ επιλέξτε την ενέργεια που επιθυμείτε να εκτελέσετε:");
 		System.out.println("1. ΚΑΘΑΡΙΣΜΟΣ ΛΙΣΤΑΣ");
 		System.out.println("2. ΔΙΑΓΡΑΦΗ ΑΠΟ ΤΟΝ ΟΡΓΑΝΙΣΜΟ");
 		System.out.println("3. ΑΚΥΡΩΣΗ");
@@ -193,7 +194,7 @@ private void monitorOrganization(Scanner sc, Admin admin, Organization org){
 					org.removeBeneficiary(selectedBen);
 				}
 				else 
-				System.out.println("Η αφαίρεση ακυρώθηκε...")
+				System.out.println("Η αφαίρεση ακυρώθηκε...");
 				break;
 			case 3:
 				break; 
@@ -222,8 +223,8 @@ private void monitorOrganization(Scanner sc, Admin admin, Organization org){
 		}
 		if ((indexDon < org.getDonatorList().size()) && (indexDon >= 0)) {
 			Donator selectedDon = org.getDonatorList().get(i);// temporary reference to the selected Donator
-			System.out.println("Συνολικές Προσφερόμενες Παροχές:")
-			for (int i = 0; i selectedDon.getOffersList().listSize(); i++) {
+			System.out.println("Συνολικές Προσφερόμενες Παροχές:");
+			for (int i = 0; i < selectedDon.getOffersList().listSize(); i++) {
 				System.out.println(selectedDon.getOffersList().getArrayList().get(i).getEntity().getName() + selectedDon.getOffersList().getArrayList().get(i).getQuantity());
 			}// end for
 
@@ -238,7 +239,7 @@ private void monitorOrganization(Scanner sc, Admin admin, Organization org){
 			System.out.println("Αφαιρέθηκε επιτυχώς!");
 		}
 		else 
-			System.out.println("Η αφαίρεση ακυρώθηκε...")
+			System.out.println("Η αφαίρεση ακυρώθηκε...");
 		break;
 		//RESET BENEFICIARIES' LISTS:
 	case 3:
